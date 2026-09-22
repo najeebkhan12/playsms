9286545a87586493b5ba30d5aa4e6b2404200d37
najeebkhan12 <khannajeeb362@gmail.com>
2026-09-22 19:59:53 +0500

    composer installed via docker image and now run composer directly

--- getcomposer.sh
+++ getcomposer.sh
@@ -3,33 +3,29 @@
 PATHSRC=$(pwd)
 
 echo
-echo "Getting composer from https://getcomposer.org"
-echo
-echo "Please wait while this script downloading composer"
+echo "Checking Composer installation..."
 echo
 
-rm -f ./composer ./composer.phar
-
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-
-if [ -e "./composer.phar" ]; then
-	ln -s ./composer.phar ./composer >/dev/null 2>&1
-	chmod +x ./composer.phar >/dev/null 2>&1
+# Verify composer exists
+if ! command -v composer >/dev/null 2>&1; then
+    echo "ERROR: Composer is not installed or not in PATH"
+    exit 1
 fi
 
-echo "Composer has been installed"
-echo
-echo "Please wait while composer getting and updating required packages"
+echo "Composer found:"
+composer --version
 echo
 
-if [ -x "./composer.phar" ]; then
-	cd "$PATHSRC"
-	./composer.phar update
-	exit $?
+echo "Installing/updating required packages..."
+echo
+
+cd "$PATHSRC"
+
+# Use install if composer.lock exists, otherwise update
+if [ -f "composer.lock" ]; then
+    composer install --no-interaction
 else
-	echo "ERROR: unable to get composer from https://getcomposer.org"
-	exit 1
+    composer update --no-interaction
 fi
+
+exit $?
